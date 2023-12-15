572e13e8bfc79ce3f53c3372b5473e69dac319ea
zoynulabedin <trustcodernet@gmail.com>
2023-12-16 00:06:14 +0600

    update

--- app/routes/dashboard.artist.jsx
+++ app/routes/dashboard.artist.jsx
@@ -8,7 +8,7 @@ const Artist = () => {
     return <Outlet />;
   }
   return (
-    <div className="flex flex-row justify-between flex-wrap">
+    <div className="grid grid-cols-6 gap-4">
       <SlideItem thumbnail="../../img/1.png" artist="PFire" id="1" />
       <SlideItem thumbnail="../../img/2.png" artist="Ice Spice" id="2" />
       <SlideItem thumbnail="../../img/3.png" artist="Neil Tran" id="3" />
@@ -17,18 +17,7 @@ const Artist = () => {
       <SlideItem thumbnail="../../img/1.png" artist="PFire" id="6" />
       <SlideItem thumbnail="../../img/2.png" artist="Ice Spice" id="7" />
       <SlideItem thumbnail="../../img/3.png" artist="Neil Tran" id="8" />
-      <SlideItem thumbnail="../../img/4.png" artist="Soo Jin Ae" id="9" />
-      <SlideItem thumbnail="../../img/5.png" artist="Yael Amari" id="10" />
-      <SlideItem thumbnail="../../img/1.png" artist="PFire" id="11" />
-      <SlideItem thumbnail="../../img/2.png" artist="Ice Spice" id="12" />
-      <SlideItem thumbnail="../../img/3.png" artist="Neil Tran" id="13" />
-      <SlideItem thumbnail="../../img/4.png" artist="Soo Jin Ae" id="14" />
-      <SlideItem thumbnail="../../img/5.png" artist="Yael Amari" id="15" />
-      <SlideItem thumbnail="../../img/1.png" artist="PFire" id="16" />
-      <SlideItem thumbnail="../../img/2.png" artist="Ice Spice" id="17" />
-      <SlideItem thumbnail="../../img/3.png" artist="Neil Tran" id="18" />
-      <SlideItem thumbnail="../../img/4.png" artist="Soo Jin Ae" id="19" />
-      <SlideItem thumbnail="../../img/5.png" artist="Yael Amari" id="20" />
+      
       <Outlet />
     </div>
   );
--- app/routes/dashboard.shop.jsx
+++ app/routes/dashboard.shop.jsx
@@ -1,13 +1,13 @@
 import Product from "../components/product/Product";
 const Shop = () => {
   return (
-    <>
+    <div className="">
       <h1 className="text-white text-lg24 mt-3">MarketPlace</h1>
       <p className="text-white text-xs16 mb-4">
         Introducing Treats Hanukkah, a festive collection featuring apparel,
         custom dreidels, and a custom Hanukkah Plate Set
       </p>
-      <div className="grid xs:grid-cols-1 sml:grid-cols-2 lgl:grid-cols-3 gap-5">
+      <div className="grid xs:grid-cols-2 sml:grid-cols-3 lgl:grid-cols-3 gap-5">
         <Product
           thumbnail="../../img/s1.png"
           price="$1500"
@@ -29,50 +29,10 @@ const Shop = () => {
           cat="Disney"
           color="Black"
         />
-        <Product
-          thumbnail="../../img/s4.png"
-          price="$1500"
-          title="Kith for Mickey & Friends Suede Jacket"
-          cat="Disney"
-          color="Black"
-        />
-        <Product
-          thumbnail="../../img/s5.png"
-          price="$1500"
-          title="Kith for Mickey & Friends Suede Jacket"
-          cat="Disney"
-          color="Black"
-        />
-        <Product
-          thumbnail="../../img/s5.png"
-          price="$1500"
-          title="Kith for Mickey & Friends Suede Jacket"
-          cat="Disney"
-          color="Black"
-        />
-        <Product
-          thumbnail="../../img/s6.png"
-          price="$1500"
-          title="Kith for Mickey & Friends Suede Jacket"
-          cat="Disney"
-          color="Black"
-        />
-        <Product
-          thumbnail="../../img/s7.png"
-          price="$1500"
-          title="Kith for Mickey & Friends Suede Jacket"
-          cat="Disney"
-          color="Black"
-        />
-        <Product
-          thumbnail="../../img/s8.png"
-          price="$1500"
-          title="Kith for Mickey & Friends Suede Jacket"
-          cat="Disney"
-          color="Black"
-        />
+   
+    
       </div>
-    </>
+    </div>
   );
 };
 export default Shop;
